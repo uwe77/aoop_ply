36dce447352ae3ab5645ca4b44f877f49c21ef2d
uwe <uwe90711@gmail.com>
2023-09-08 16:44:49 +0800

    tring too create a npc with lex

---
+++ npc.py
@@ -0,0 +1,24 @@
+import ply.lex as lex
+
+tokens = [
+    "PLUS",
+    "MINUS",
+    "NUMBER",
+    ]
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+def t_NUMBER(t):
+    r'\d+'
+    pass
+
+def t_NUMBER(t):
+    r'\d+'
+    pass
+
+def t_error(t):
+    pass
+
+
+
+lex.lex()
