49fe5dfc0e33249452d2789ff20ca2ac6c7217f5
uwe <uwe90711@google.com>
2023-09-12 23:38:21 +0800

    yes

--- npc.py
+++ npc.py
@@ -3,70 +3,85 @@ import ply.yacc as yacc
 
 # Define lexer tokens
 tokens = (
-    'KEY',
-    'EQUALS',
-    'VALUE',
-    'NEWLINE',
+    'MISSION'
 )
 
-# Lexer rules
-t_EQUALS = r'='
-t_ignore = ' \t'
+literals = ['=', '+', '-', '*', '(', ')'] #u can use tokens and re to do the same thing
 
-def t_KEY(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = 'KEY'
+def t_MISSION(t):
+    r'\w+'
     return t
 
-def t_VALUE(t):
-    r'[^=\n]+'
-    return t
+t_ignore = " \t"
 
-def t_NEWLINE(t):
+def t_newline(t):
     r'\n+'
-    t.lexer.lineno += t.value.count('\n')
-    return t
+    t.lexer.lineno += t.value.count("\n")
 
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+    print(f"Illegal charater {t.value[0]}")
     t.lexer.skip(1)
 
+# build the lexer
 lexer = lex.lex()
 
-# # Parser rules
-def p_config(p):
-    '''
-    config : config key_value
-           | key_value
-    '''
-    if len(p) == 3:
-        p[0] = p[1] + [p[2]]
-    else:
-        p[0] = [p[1]]
+# Parsing rules
 
-def p_key_value(p):
-    'key_value : KEY EQUALS VALUE NEWLINE'
-    p[0] = (p[1], p[3])
+precedence = (
+    ('left', '+', '-'),
+    ('left', '*'),
+    ('right', 'UMINUS'),
+)
+
+# dictionary of names
+names = {}
+
+def p_statement_assign(p):
+    'statement : MISSION "=" expression'
+    names[p[1]] = p[3]
+
+def p_statement_expr(p):
+    'statement : expression'
+    print(p[1])
+
+def p_expression_binop(p):
+    '''expression : expression '+' expression
+                  | expression '-' expression
+                  | expression '*' expression'''
+    if p[2] == '+':
+        p[0] = f"im going to do {p[1]}, then {p[3]}"
+    elif p[2] == '-':
+        p[0] = f"im goint to do {p[1]} without {p[3]}"
+    elif p[2] == '*':
+        p[0] = f"im going to do {p[1]} and {p[3]} at the same time"
+
+# def p_expression_uminus(p):
+
+def p_expression_group(p):
+    "expression : '('  expression ')'"
+    p[0] = p[2]
+
+def p_expression_mission(p):
+    "expression : MISSION"
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = ' '
 
 def p_error(p):
-    print(f"Syntax error at line {p.lineno}: {p.value}")
-
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
 
 parser = yacc.yacc()
 
-# Sample configuration text
-config_text = """
-name = John
-age = 30
-email = john@example.com
-"""
-
-# Parse the configuration
-config = parser.parse(config_text.strip())
-
-# Print the parsed configuration
-try:
-    for key, value in config:
-        print(f"{key}: {value}")
-except TypeError:
-    print("Error: Invalid configuration format.")
+while True:
+    try:
+        s = input('I want to... ')
+    except EOFError:
+        break
+    if not s:
+        continue
+    yacc.parse(s)
--- test.py
+++ test.py
@@ -1,3 +1,72 @@
-p = [10,20,30,40]
-result = [p[2]]
-print(result, "and long: ", len(result))
+import ply.lex as lex
+import ply.yacc as yacc
+
+# Define lexer tokens
+tokens = (
+    'KEY',
+    'EQUALS',
+    'VALUE',
+    'NEWLINE',
+)
+
+# Lexer rules
+t_EQUALS = r'='
+t_ignore = ' \t'
+
+def t_KEY(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = 'KEY'
+    return t
+
+def t_VALUE(t):
+    r'[^=\n]+'
+    return t
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count('\n')
+    return t
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+# # Parser rules
+def p_config(p):
+    '''
+    config : config key_value
+           | key_value
+    '''
+    if len(p) == 3:
+        p[0] = p[1] + [p[2]]
+    else:
+        p[0] = [p[1]]
+
+def p_key_value(p):
+    'key_value : KEY EQUALS VALUE NEWLINE'
+    p[0] = (p[1], p[3])
+
+def p_error(p):
+    print(f"Syntax error at line {p.lineno}: {p.value}")
+
+
+parser = yacc.yacc()
+
+# Sample configuration text
+config_text = """
+name = John
+age = 30
+email = john@example.com
+"""
+
+# Parse the configuration
+config = parser.parse(config_text.strip())
+
+# Print the parsed configuration
+try:
+    for key, value in config:
+        print(f"{key}: {value}")
+except TypeError:
+    print("Error: Invalid configuration format.")
