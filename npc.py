7ae268a62783f4d029a23d4ddb8b8d0e3a8b723e
uwe <uwe90711@gmail.com>
2023-09-28 13:36:19 +0800

    modify

--- npc.py
+++ npc.py
@@ -1,99 +1,59 @@
 import ply.lex as lex
 import ply.yacc as yacc
-# import re
-class NPC:
-    # NPC's name, age, gender
-    # def __init__(self) -> None:
-    #     self._name_match = r'[a-zA-Z]+'
-    #     self.reserved = {
-    #     'my name is' : 'my_name_is',
-    #     }
+tokens = ( 'NAME',
+           'ADD', 
+           'TEAM', 
+           'SEMICOLON',
+            )
+
+t_ADD = r'\+'
+
+t_SEMICOLON = r';'
+def t_NAME(t):
+    r'[a-zA-Z]+'
+    t.type = 'NAME'
+    return t
+# Define a rule so we can track name
+def t_TEAM(t):
+    r'[a-zA-Z]+:'
+    t.value = t.value[:-1]
+    t.type = 'TEAM' return t
+# define a rule so we can track group
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+
+def p_expression_add(p):
+    'expression : expression ADD term'
+    
+def p_expression_semicolon(p):
+    'expression : expression SEMICOLON term'
+
+def p_factor_name(p):
+    'factor : NAME'
+    
+def p_factor_team(p):
+    'factor : TEAM'
+    
+def p_error(p):
+    print("Systax error in input!")
         
-    ### building token=============================================
-
-    tokens = (
-        #declear your tokens type
-        'NAME',
-        'AGE',
-        'MY',
-        'YOUR',
-    )
-    # define your tokens
-    t_YOUR = r'[y, Y]our'
-
-    t_MY = r'[m, M]y'
-
-    def t_NAME(self, t):
-        r'name_is_[a-zA-Z]+'
-        t.value = t.value[8:]
-        t.type = 'NAME'
-        return t
-        # Define a rule so we can track name
-    
-    def t_AGE(self, t):
-        r'age_is_\d+'
-        t.value = t.value[7:]
-        t.value = int(t.value)
-        t.type = 'AGE'
-        return t
-        # Define a rule so we can track name
-
-    def t_newline(self,t):
-        r'\n+'
-        t.lexer.lineno += len(t.value)
-        # A string containing ignored characters (spaces and tabs)
-
-    t_ignore = ' \t'
-    # Error handling rule
-
-    def t_error(self,t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
-        # Build the lexer
-    ###============================================================
-    
-    ###building parser=============================================
-    # def p_expression_my(p):
-    #     'expression : MY term'
-    #     p[0] = f'my name is {p[2]}'
-    # def p_expression_name(p):
-    #     'factor : NAME'
-    #     # p[0] = p[1]
-        
-    # def p_expression_age(p):
-    #     'factor : AGE'
-    #     p[0] = p[1]
-
-    # def p_term_factor(p):
-    #     'term : factor'
-    #     p[0] = p[1]
-
-    # def p_expression_term(p):
-    #     'expression : term'
-    #     p[0] = p[1]
-
-    # def p_error(p):
-    #     print("Systax error in input!")
-    ###============================================================
-    def build_lexer(self,**kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
-        # self.parser = yacc.yacc(module=self, **kwargs)
-        # Test it output
-
-    # def build_parser(self):
-    #     self.parser = yacc.yacc()
-
-    def test_token(self,data):
-        self.lexer.input(data)
-        while True:
-            tok = self.lexer.token()
-            if not tok:
-                break
-            print(tok)
-    
-    # def test_parser(self, **kwargs):
-
-n = NPC()
-n.build_lexer()
-# n.build_parser()
-n.test_token("my name_is_uwe and my age_is_22")
+if __name__ == '__main__':
+    lexer = lex.lex()
+    parser = yacc.yacc()
+    while True:
+        try:
+            s = input('input: ')
+        except EOFError:
+            break
+        if not s:
+            continue
+        result = parser.parse(s)
+        print(result)
